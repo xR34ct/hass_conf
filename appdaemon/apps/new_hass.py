46b06cdcbdc92b32e7b66f6397bbbd6425e693c2
xR34ct <avdagic94@gmail.com>
2018-10-09 00:22:37 +0200

    Changed to actually track updates....

--- appdaemon/apps/new_hass.py
+++ appdaemon/apps/new_hass.py
@@ -4,8 +4,8 @@ import appdaemon.plugins.hass.hassapi as hass
 class HassUpdate(hass.Hass):
 
   def initialize(self):
-     self.listen_state(self.message, "binary_sensor.test")
      self.log("Started update monitoring")
+     self.listen_state(self.message, "updater.updater")
   def message (self, entity, attribute, old, new, kwargs):
      self.call_service("notify/pushbullet", title = "Home Assistant", message = "There is an update to Homeassistant")
      self.log("Notification sent for new update")
